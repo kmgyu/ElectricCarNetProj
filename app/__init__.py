740f44053d98c77f171122b49cf24bcd0a5c789f
kmgyu <lidersy961@gmail.com>
2024-12-11 17:11:59 +0900

    init test

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from .views import main_views
+    from views import main_views
     app.register_blueprint(main_views.bp)
 
     return app
+
+if __name__ == '__main__':
+    create_app().run()
